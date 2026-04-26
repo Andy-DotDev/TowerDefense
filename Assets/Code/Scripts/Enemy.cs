e4aab0c143ea61316186698c968ae58b5773b0da
Andy-DotDev <colotilin.an@yandex.ru>
2026-04-26 19:32:53 +0300

    Map and Enemies
    Added:
    - 3 types enemies
    - Logic enemies movement
    - TileSet

---
+++ Assets/Code/Scripts/Enemy.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class Enemy: MonoBehaviour
+{
+    private SpriteRenderer sr;
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+
+    [Header("Attributes")]
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float flipThreshold = 0.1f;
+    [SerializeField] private float minMoveThreshold = 0.01f;
+
+    private Transform target;
+    private int pathIndex = 0;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+    private void Start()
+    {
+        target = EnemyManager.main.path[pathIndex];
+    }
+
+    private void Update()
+    {
+
+
+        if (Vector2.Distance(target.position, transform.position) <= 0.1f) { 
+            pathIndex++;
+            if (pathIndex >= EnemyManager.main.path.Length)
+            {
+                EnemySpawner.onEnemyDestroy.Invoke();
+                Destroy(gameObject);
+                return;
+            }
+            else {
+                target = EnemyManager.main.path[pathIndex];
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = (target.position - transform.position).normalized;
+
+        if (direction.magnitude > minMoveThreshold) {
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            angle = Mathf.Round(angle / 90f) * 90f;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+           
+
+        rb.linearVelocity = direction * moveSpeed;
+    }
+}
